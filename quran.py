dc7236c2f0d101cca7133a20c7671ae3a1818624
olinssar <143740876+olinssar@users.noreply.github.com>
2023-11-24 16:13:55 +0300

    Add files via upload

---
+++ quran.py
@@ -0,0 +1,276 @@
+from pytgcalls import PyTgCalls
+from pyrogram import Client
+from pytgcalls import idle
+from pytgcalls.types import AudioPiped
+from config import *
+
+
+client = Client('t', API_ID, API_HASH, session_string=SESSION_STRING)
+
+client1 = Client('t1', API_ID1, API_HASH1, session_string=SESSION_STRING1)
+client2 = Client('t2', API_ID2, API_HASH2, session_string=SESSION_STRING2)
+client3 = Client('t3', API_ID3, API_HASH3, session_string=SESSION_STRING3)
+client4 = Client('t4', API_ID4, API_HASH4, session_string=SESSION_STRING4)
+client5 = Client('t5', API_ID5, API_HASH5, session_string=SESSION_STRING5)
+
+client6 = Client('t6', API_ID6, API_HASH6, session_string=SESSION_STRING6)
+client7 = Client('t7', API_ID7, API_HASH7, session_string=SESSION_STRING7)
+client8 = Client('t8', API_ID8, API_HASH8, session_string=SESSION_STRING8)
+client9 = Client('t9', API_ID9, API_HASH9, session_string=SESSION_STRING9)
+client10= Client('t10', API_ID10, API_HASH10, session_string=SESSION_STRING10)
+
+client11 = Client('t11', API_ID11, API_HASH11, session_string=SESSION_STRING11)
+client12 = Client('t12', API_ID12, API_HASH12, session_string=SESSION_STRING12)
+client13 = Client('t13', API_ID13, API_HASH13, session_string=SESSION_STRING13)
+client14 = Client('t14', API_ID14, API_HASH14, session_string=SESSION_STRING14)
+
+
+client15= Client('tq0', API_ID15, API_HASH15, session_string=SESSION_STRING15)
+
+client16 = Client('t51', API_ID16, API_HASH16, session_string=SESSION_STRING16)
+client17 = Client('t62', API_ID17,
+API_HASH17, session_string=SESSION_STRING17)
+
+client18 = Client('t73', API_ID18, API_HASH18, session_string=SESSION_STRING18)
+client19 = Client('t84', API_ID19, API_HASH19, session_string=SESSION_STRING19)
+
+app = PyTgCalls(client)
+app1 = PyTgCalls(client1)
+app2 = PyTgCalls(client2)
+app3 = PyTgCalls(client3)
+app4 = PyTgCalls(client4)
+app5 = PyTgCalls(client5)
+app6 = PyTgCalls(client6)
+app7 = PyTgCalls(client7)
+app8 = PyTgCalls(client8)
+app9 = PyTgCalls(client9)
+app10 = PyTgCalls(client10)
+app11 = PyTgCalls(client11)
+app12 = PyTgCalls(client12)
+app13 = PyTgCalls(client13)
+app14 = PyTgCalls(client14)
+
+app15 = PyTgCalls(client15)
+app16 = PyTgCalls(client16)
+app17 = PyTgCalls(client17)
+app18 = PyTgCalls(client18)
+app19 = PyTgCalls(client19)
+
+
+app.start()
+app.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app1.start()
+app1.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+
+app2.start()
+app2.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app3.start()
+app3.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app4.start()
+app4.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app5.start()
+app5.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app6.start()
+app6.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app7.start()
+app7.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app8.start()
+app8.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app9.start()
+app9.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app10.start()
+app10.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app11.start()
+app11.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+
+app12.start()
+app12.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app13.start()
+app13.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app14.start()
+app14.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+
+
+app15.start()
+app15.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app16.start()
+app16.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+
+app17.start()
+app17.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+app18.start()
+app18.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+app19.start()
+app19.join_group_call(
+    -1001826010858,
+    AudioPiped('https://archive.org/details/20200310_20200310_1015/%D8%A7%D8%AB%D8%A8%D8%AA%D9%88%D8%A7+%D9%8A%D8%A7+%D8%A3%D9%87%D9%84+%D8%A5%D8%AF%D9%84%D8%A8.mp3')
+
+)
+
+
+app.mute_stream(
+ -1001826010858,
+)
+
+
+app1.mute_stream(
+ -1001826010858,
+)
+app2.mute_stream(
+ -1001826010858,
+)
+
+app3.mute_stream(
+ -1001826010858,
+)
+app4.mute_stream(
+ -1001826010858,
+)
+
+app5.mute_stream(
+ -1001826010858,
+)
+
+app6.mute_stream(
+ -1001826010858,
+)
+
+app7.mute_stream(
+ -1001826010858,
+)
+
+app8.mute_stream(
+ -1001826010858,
+)
+
+app9.mute_stream(
+ -1001826010858,
+)
+
+app10.mute_stream(
+ -1001826010858,
+)
+app11.mute_stream(
+ -1001826010858,
+)
+app12.mute_stream(
+ -1001826010858,
+)
+
+app13.mute_stream(
+ -1001826010858,
+)
+app14.mute_stream(
+ -1001826010858,
+)
+
+
+app15.mute_stream(
+ -1001826010858,
+)
+app16.mute_stream(
+ -1001826010858,
+)
+app17.mute_stream(
+ -1001826010858,
+)
+
+app18.mute_stream(
+ -1001826010858,
+)
+app19.mute_stream(
+ -1001826010858,
+)
+
+
+
+idle()
